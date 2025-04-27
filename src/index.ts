f61a6e34c7492b1bf066021118502e630b4d0e80
Jakub T. Jankiewicz <jcubic@onet.pl>
2025-04-27 19:29:11 +0200

    repro

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import jQuery from 'jquery';
 type JQueryStaticOriginal = JQueryStatic;
 import terminal from 'jquery.terminal';
-
+import type { JQueryTerminal, JQueryStatic } from 'jquery.terminal';
 
 function foo(jQuery: JQueryStaticOriginal) {
     return jQuery;
